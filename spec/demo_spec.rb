5f6c99dc4d2cea39c776cd91ea008ef00de17acc
Mike Williams <mdub@dogbiscuit.org>
2011-03-31 11:51:30 +1100

    Checkpoint: example of (useful) failing test.

--- spec/demo_spec.rb
+++ spec/demo_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "demo page" do
   
-  before do
+  before(:all) do
     webdriver.navigate.to(fixture_url("demo.html"))
   end
   
@@ -11,8 +11,7 @@ describe "demo page" do
     let(:h1) { webdriver.find_element(:css, 'h1') }
     subject { h1 }
   
-    it { should have_color("#000800") }
-    it { should have_position(:x => 5) }
+    it { should have_color("#000000") }
 
     describe "text" do
       subject { h1.text }
@@ -20,5 +19,22 @@ describe "demo page" do
     end
 
   end
+
+  let(:main) { webdriver.find_element(:css, '#main') }
+  let(:sidebar) { webdriver.find_element(:css, 'aside') }
+
+  describe "#main" do
+
+    it "is on the left of the window" do
+      main.location.x.should <= 30
+    end
+    
+  end
+
+  # describe "sidebar" do
+  #   
+  #   it { should be_right_of("#main") }
+  #   
+  # end
   
 end
